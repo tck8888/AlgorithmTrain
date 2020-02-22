5dbced962c2e76f1d5dcd1f4cf76e7dd67ad6122
tck8888 <l>
2020-02-22 23:09:38 +0800

    落谷刷题票P1000

---
+++ src/main/java/com/tck/algorithm/huffman/HuffmanDemo.java
@@ -0,0 +1,21 @@
+package com.tck.algorithm.huffman;
+
+/**
+ * 哈夫曼算法
+ * <p>
+ * 日志:
+ * 美团logan    https://github.com/Meituan-Dianping/Logan/tree/master/Logan/Clogan
+ * 腾讯xlog
+ * <p>
+ * <p>
+ * 压缩算法 gzip http bsdiff
+ * LZW huffman
+ * <p>
+ * SKJpegEncoder.cpp
+ */
+public class HuffmanDemo {
+
+
+    public static void main(String[] args)  {
+    }
+}
