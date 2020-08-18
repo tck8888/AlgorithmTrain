f1ffae33c5ad5670bd51abdd3c57d5c2a58dfb00
tck <tck@dra100.com>
2020-08-18 09:10:33 +0800

    有序链表转换二叉搜索树

---
+++ src/main/java/com/tck/algorithm/leetcode/Train109.java
@@ -0,0 +1,87 @@
+package com.tck.algorithm.leetcode;
+
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
+/**
+ * https://leetcode-cn.com/problems/convert-sorted-list-to-binary-search-tree/
+ */
+public class Train109 {
+
+    //有序链表转换二叉搜索树
+
+    ListNode globalHead;
+
+    public TreeNode sortedListToBST(ListNode head) {
+        globalHead = head;
+        int length = getLength(head);
+        return buildTree(0, length - 1);
+    }
+
+    public int getLength(ListNode head) {
+        int ret = 0;
+        while (head != null) {
+            ++ret;
+            head = head.next;
+        }
+        return ret;
+    }
+
+    public TreeNode buildTree(int left, int right) {
+        if (left > right) {
+            return null;
+        }
+        int mid = (left + right + 1) / 2;
+        TreeNode root = new TreeNode();
+        root.left = buildTree(left, mid - 1);
+        root.val = globalHead.val;
+        globalHead = globalHead.next;
+        root.right = buildTree(mid + 1, right);
+        return root;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+}
